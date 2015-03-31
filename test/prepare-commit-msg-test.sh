d89e351f6d8cf300ef35a08afec2ee6bded69164
HackMate <HackMate@users.noreply.github.com>
2015-03-31 15:41:41 +0200

    Updated prepare-commit-msg hook to parse lowercase issues.

--- test/prepare-commit-msg-test.sh
+++ test/prepare-commit-msg-test.sh
@@ -12,11 +12,9 @@ assert "parse_issue_key invalid_commit_msg_without_issue"   ""
 assert "parse_issue_key prefix/MY_EXAMPLE_PROJECT-9999"     "MY_EXAMPLE_PROJECT-9999"
 assert "parse_issue_key PROJECT-200-postfix"                "PROJECT-200"
 assert "parse_issue_key 2013PROJECT-200"                    "PROJECT-200"
-assert "parse_issue_key valid_PRODUCT_2013-234"             "PRODUCT_2013-234"
-assert "parse_issue_key invalid_loCA-200"                   "CA-200"
-assert "parse_issue_key valid_R2D2-876"                     "R2D2-876"
+assert "parse_issue_key PRODUCT_2013-234"                   "PRODUCT_2013-234"
+assert "parse_issue_key loCA-200"                           "loCA-200"
 assert "parse_issue_key invalid_FÖ-1"                       ""
-assert "parse_issue_key valid_FD-100"                       "FD-100"
 
 # Carefull, this will only work on the master!
 skip_if "! git --version"
